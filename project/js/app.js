942de1a65704439ff01c4df132fd4fb2f9df205a
MyoMyintAung <myomyintaung064@gmail.com>
2022-01-09 09:58:19 +0630

    Complete the exchange logic

--- project/js/app.js
+++ project/js/app.js
@@ -1,5 +1,7 @@
-const from = document.querySelector("#from");
-const to = document.querySelector("#to");
+const inputEle = document.querySelector("#input");
+const fromEle = document.querySelector("#from");
+const toEle = document.querySelector("#to");
+const resultEle = document.querySelector(".result-text");
 
 function toNum(x) {
   return Number(x.replace(",", ""));
@@ -12,8 +14,27 @@ function appendOption(place) {
     option.setAttribute("id", i);
     option.setAttribute("value", toNum(data.rates[i]));
     place.append(option);
+    console.log(i);
   }
 }
 
 appendOption(from);
 appendOption(to);
+
+document.querySelector("#input-section").addEventListener("submit", (e) => {
+  e.preventDefault();
+  let input = inputEle.value;
+  let from = fromEle.value;
+  let to = toEle.value;
+  console.log(input, from, to);
+
+  let first = input * from;
+  let final = first / to;
+  console.log(final);
+
+  resultEle.innerHTML = final.toFixed(2);
+  inputEle.value = "";
+  fromEle.value = "";
+  toEle.value = "1";
+  inputEle.focus = true;
+});
