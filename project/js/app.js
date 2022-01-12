34bcdc6b5f3b66f68f3af9981c2945a5ed9332e9
MyoMyintAung <myomyintaung064@gmail.com>
2022-01-12 13:16:15 +0630

    Add history feature

--- project/js/app.js
+++ project/js/app.js
@@ -2,18 +2,22 @@ const inputEle = document.querySelector("#input");
 const fromEle = document.querySelector("#from");
 const toEle = document.querySelector("#to");
 const resultEle = document.querySelector(".result-text");
+const tbody = document.querySelector("#historyList");
 
 function toNum(x) {
   return Number(x.replace(",", ""));
 }
 
 function appendOption(place) {
+  const optgroup = document.createElement("optgroup");
+  optgroup.setAttribute("id", "opt-group");
   for (let i in data.rates) {
     const option = document.createElement("option");
     option.innerText = i;
     option.setAttribute("id", i);
     option.setAttribute("value", toNum(data.rates[i]));
-    place.append(option);
+    optgroup.append(option);
+    place.append(optgroup);
     console.log(i);
   }
 }
@@ -21,20 +25,52 @@ function appendOption(place) {
 appendOption(from);
 appendOption(to);
 
+function createList(array) {
+  let tr = document.createElement("tr");
+
+  array.map((el) => {
+    let td = document.createElement("td");
+    let text = document.createTextNode(el);
+    td.append(text);
+    tr.append(td);
+    tbody.append(tr);
+  });
+}
+
+function store() {
+  localStorage.setItem("record", tbody.innerHTML);
+}
+
 document.querySelector("#input-section").addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // Get State
   let input = inputEle.value;
   let from = fromEle.value;
   let to = toEle.value;
-  console.log(input, from, to);
 
+  //process state
+  let fromDisplay = input + " " + fromEle[fromEle.selectedIndex].innerText;
+  let toDisplay = toEle[toEle.selectedIndex].innerText;
   let first = input * from;
   let final = first / to;
-  console.log(final);
+  let result = final.toFixed(2);
+  let date = new Date().toLocaleString();
+  let arr = [date, fromDisplay, toDisplay, result + " " + toDisplay];
 
-  resultEle.innerHTML = final.toFixed(2);
+  // set state
+  resultEle.innerHTML = result;
   inputEle.value = "";
   fromEle.value = "";
   toEle.value = "1";
   inputEle.focus = true;
+
+  createList(arr);
+  store();
 });
+
+(function () {
+  if (localStorage.getItem("record")) {
+    tbody.innerHTML = localStorage.getItem("record");
+  }
+})();
